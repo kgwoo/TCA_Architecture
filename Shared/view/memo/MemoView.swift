4c9aaaf1420451d6a66bd40610d686fd2d94ea05
gunoo <gunoo@gimgeoncBookPro.kornet>
2022-08-23 11:27:46 +0900

    폴더구조세팅

---
+++ Shared/view/memo/MemoView.swift
@@ -0,0 +1,22 @@
+//
+//  MemoView.swift
+//  TCA_Architecture (iOS)
+//
+//  Created by gunoo on 2022/08/23.
+//
+
+import SwiftUI
+
+struct MemoView: View {
+    var body: some View {
+        NavigationView{
+            Text("메모")
+        }
+    }
+}
+
+struct MemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoView()
+    }
+}
